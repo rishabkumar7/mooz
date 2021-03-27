c73e5e4a53fba854d0ddc9b3318614a3cdfa443d
Rishab Kumar <rishabkumar7@gmail.com>
2021-03-27 17:00:38 -0400

    added http server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+const { response } = require('express');
 const express = require('express');
+const { request } = require('node:http');
 const app = express();
+const server = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello World");
+})
+
+
+
+
+
+
+server.listen(3030);
