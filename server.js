c18b67b49856c148104b9100d8971569498ba155
Rishab Kumar <rishabkumar7@gmail.com>
2021-03-27 23:09:48 -0400

    created room view and implemented uuid

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const { response } = require('express');
 const express = require('express');
-const { request } = require('node:http');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World");
+    res.redirect(`/${uuidv4()}`);
 })
 
-
-
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
 
 
 
